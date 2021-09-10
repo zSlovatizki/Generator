8403e3b32b533b6ba5c5a307626125dd7eec95be
zipi <zipislova@gmail.com>
2021-09-10 14:51:45 +0300

    - delete

--- project/src/Map.js
+++ project/src/Map.js
@@ -17,15 +17,16 @@ import PlacesAutocomplete, {
 import { Select } from '@material-ui/core';
 
 class Map extends Component {
+
   constructor(props) {
     super(props);
-
     this.state = {
       isOpen: false,
-      coords:   { lat: 18.55996 , lng: -68.388832 },
+      coords: { lat: 18.55996, lng: -68.388832 },
       address: 'search',
     };
   }
+
   handleChange = address => {
     this.setState({ address });
     console.log("handlechange")
@@ -35,7 +36,6 @@ class Map extends Component {
     console.log("handleselect")
     console.log(this.state.address);
     console.log(this.state.coords);
-
     console.log("handleselect")
 
     geocodeByAddress(address)
@@ -46,15 +46,13 @@ class Map extends Component {
         })
       )
       .catch(error => console.error('Error', error));
-      this.setState({
-        address:address
-
-      })
-      console.log("handleselect")
-      console.log("address state:",address)
-      console.log(this.state.coords);
-
-      console.log("handleselect")
+    this.setState({
+      address: address
+    })
+    console.log("handleselect")
+    console.log("address state:", address)
+    console.log(this.state.coords);
+    console.log("handleselect")
   };
 
   handleToggleOpen = () => {
@@ -68,13 +66,13 @@ class Map extends Component {
       isOpen: false
     });
   };
- style={
-  border: "#282c34 solid 1px"
-}
- path = [
-  { lat: 32.0855141 , lng:34.8441714 },
-  { lat: 32.0859428 , lng:34.8442113  },
-];
+  style = {
+    border: "#282c34 solid 1px"
+  }
+  path = [
+    { lat: 32.0855141, lng: 34.8441714 },
+    { lat: 32.0859428, lng: 34.8442113 },
+  ];
   render() {
     const GoogleMapExample = withGoogleMap(props => (
       <GoogleMap defaultCenter={this.state.coords} defaultZoom={13}>
@@ -93,12 +91,11 @@ class Map extends Component {
           )}
         </Marker>
         <Polyline path={this.path} options={{ strokeColor: "#FF0000 " }} />
-
       </GoogleMap>
     ));
-    const styleWidth={
-      width:"100%"
 
+    const styleWidth = {
+      width: "100%"
     }
     return (
       <div>
@@ -113,47 +110,45 @@ class Map extends Component {
             getSuggestionItemProps,
             loading
           }) => (
-            <div>
-
-              <input
-                {...getInputProps({
-                  //placeholder: this.state.address,
-                  // className: 'location-search-input'
-                })}
-                text={this.state.address}
-                placeholder={this.state.address}
-              />
-              <div className="autocomplete-dropdown-container" style={this.style}>
-                {loading && <div>Loading...</div>}
-                {suggestions.map(suggestion => {
-                  const className = suggestion.active
-                    ? 'suggestion-item--active'
-                    : 'suggestion-item';
-                  // inline style for demonstration purpose
-                  const style = suggestion.active
-                    ? { backgroundColor: '#fafafa', cursor: 'pointer' }
-                    : { backgroundColor: '#ffffff', cursor: 'pointer' };
-                  return (
-                    <MenuItem
-                      {...getSuggestionItemProps(suggestion, {
-                        className,
-                        style
-                      })}
-                      key={suggestion.placeId}
-                    >
-                      <span>{suggestion.description}</span>
-                    </MenuItem>
-                  );
-                })}
+              <div>
+                <input
+                  {...getInputProps({
+                    //placeholder: this.state.address,
+                    // className: 'location-search-input'
+                  })}
+                  text={this.state.address}
+                  placeholder={this.state.address}
+                />
+                <div className="autocomplete-dropdown-container" style={this.style}>
+                  {loading && <div>Loading...</div>}
+                  {suggestions.map(suggestion => {
+                    const className = suggestion.active
+                      ? 'suggestion-item--active'
+                      : 'suggestion-item';
+                    // inline style for demonstration purpose
+                    const style = suggestion.active
+                      ? { backgroundColor: '#fafafa', cursor: 'pointer' }
+                      : { backgroundColor: '#ffffff', cursor: 'pointer' };
+                    return (
+                      <MenuItem
+                        {...getSuggestionItemProps(suggestion, {
+                          className,
+                          style
+                        })}
+                        key={suggestion.placeId}
+                      >
+                        <span>{suggestion.description}</span>
+                      </MenuItem>
+                    );
+                  })}
+                </div>
               </div>
-            </div>
-          )}
+            )}
         </PlacesAutocomplete>
         <GoogleMapExample
-        style={this.styleWidth}
+          style={this.styleWidth}
           containerElement={<div style={{ height: `500px`, width: '100%' }} />}
           mapElement={<div style={{ height: `100%` }} />}
-
         />
       </div>
     );
@@ -161,145 +156,3 @@ class Map extends Component {
 }
 
 export default Map;
-
-
-
-// import MenuItem from '@material-ui/core/MenuItem';
-// import React, { Component, useEffect, useState } from 'react';
-// import {
-//   withGoogleMap,
-//   withScriptjs,
-//   GoogleMap,
-//   Marker,
-//   InfoWindow
-// } from 'react-google-maps';
-
-// import PlacesAutocomplete, {
-//   geocodeByAddress,
-//   getLatLng
-// } from 'react-places-autocomplete';
-
-// const Map = () => {
-
-//   const [isOpen, setIsOpen] = useState(false);
-//   const [coords, setCoords] = useState({ lat: 40.756795, lng: -73.954298 });
-//   const [address, setAddress] = useState("search");
-
-//   function handleChange(address) {
-//     console.log("handlechange")
-//     setAddress(address);
-//     console.log(address)
-//     console.log("handlechange")
-//   };
-
-//   function handleSelect(address) {
-//     console.log("handleselect")
-//     console.log(address);
-//     console.log(coords);
-
-//     geocodeByAddress(address)
-//       .then(results => getLatLng(results[0]))
-//       .then(latLng =>
-//         setCoords({
-//           latLng
-//         })
-//       )
-//       .catch(error => console.error('Error', error));
-//     setAddress({
-//       address
-//     })
-//     console.log("address state:", address)
-//     console.log(coords);
-//     console.log("handleselect")
-//   };
-
-//   function handleToggleOpen() {
-//     setIsOpen(
-//       true
-//     );
-//   };
-
-//   const style = {
-//     border: "#282c34 solid 1px"
-//   }
-
-//   const GoogleMapExample = withGoogleMap(props => (
-//     <GoogleMap defaultCenter={coords} defaultZoom={13}>
-//       <Marker
-//         key={props.index}
-//         position={coords}
-//         onClick={() => handleToggleOpen()}
-//       >
-//         {isOpen && (
-//           <InfoWindow
-//             onCloseClick={props.handleCloseCall}
-//             options={{ maxWidth: 100 }}
-//           >
-//             <span>This is InfoWindow message!</span>
-//           </InfoWindow>
-//         )}
-//       </Marker>
-//     </GoogleMap>
-//   ));
-//   const styleWidth = {
-//     width: "100%"
-
-//   }
-//   return (
-//     <div>
-//       <PlacesAutocomplete
-//         value={address}
-//         onChange={handleChange}
-//         onSelect={handleSelect}
-//       >
-//         {({
-//           getInputProps,
-//           suggestions,
-//           getSuggestionItemProps,
-//           loading
-//         }) => (
-//             <div>
-
-//               <input
-//                 {...getInputProps({
-//                 })}
-//                 placeholder={address.value}
-//               />
-//               <div className="autocomplete-dropdown-container" style={style}>
-//                 {loading && <div>Loading...</div>}
-//                 {suggestions.map(suggestion => {
-//                   const className = suggestion.active
-//                     ? 'suggestion-item--active'
-//                     : 'suggestion-item';
-//                   // inline style for demonstration purpose
-//                   const style = suggestion.active
-//                     ? { backgroundColor: '#fafafa', cursor: 'pointer' }
-//                     : { backgroundColor: '#ffffff', cursor: 'pointer' };
-//                   return (
-//                     <MenuItem
-//                       {...getSuggestionItemProps(suggestion, {
-//                         className,
-//                         style
-//                       })}
-//                       key={suggestion.placeId}
-//                     //onClick={hsandleClick}
-//                     >
-//                       <span>{suggestion.description}</span>
-//                     </MenuItem>
-//                   );
-//                 })}
-//               </div>
-//             </div>
-//           )}
-//       </PlacesAutocomplete>
-//       <GoogleMapExample
-//         style={styleWidth}
-//         containerElement={<div style={{ height: `500px`, width: '100%' }} />}
-//         mapElement={<div style={{ height: `100%` }} />}
-//       />
-//     </div>
-//   );
-// }
-
-
-// export default Map;
