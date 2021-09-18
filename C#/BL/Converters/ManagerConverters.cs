03fc75d8f794d6db2669ae53f943edd706f1c9ab
zipi <zipislova@gmail.com>
2021-09-18 22:14:54 +0300

    - initial c#

---
+++ C#/BL/Converters/ManagerConverters.cs
@@ -0,0 +1,46 @@
+﻿using DAL;
+using DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.Converters
+{
+   public class ManagerConverters
+    {
+        public static ManagerDTO ConvertToDTO(T_managers manager)
+        {
+            ManagerDTO newManager = new ManagerDTO();
+            newManager.Email = manager.Email;
+            newManager.menagerId = manager.menagerId;
+            newManager.password = manager.password;
+            return newManager;
+        }
+
+        public static T_managers ConvertFromDTO(ManagerDTO manager)
+        {
+            T_managers newManager = new T_managers();
+            newManager.Email = manager.Email;
+            newManager.menagerId = manager.menagerId;
+            newManager.password = manager.password;
+            return newManager;
+        }
+        public static IEnumerable<ManagerDTO> ConvertListToDTO(List<T_managers> managersList)
+        {
+            foreach (T_managers manager in managersList)
+            {
+                yield return ConvertToDTO(manager);
+            }
+        }
+
+        public static IEnumerable<T_managers> ConvertListFromDTO(List<ManagerDTO> managerList)
+        {
+            foreach (ManagerDTO manager in managerList)
+            {
+                yield return ConvertFromDTO(manager);
+            }
+        }
+    }
+}
---
+++ C#/DTO/UserDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class UserDTO
+    {
+        public int userId { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string address { get; set; }
+        public string phone { get; set; }
+        public string Email { get; set; }
+        public double ampereAmount { get; set; }
+        public string password { get; set; }
+        public int generatorId { get; set; }
+        public int status { get; set; }
+    }
+}
