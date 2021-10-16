435512b9826c0c2d3cebc9782a4690c7080aa8d8
zipi <zipislova@gmail.com>
2021-10-17 00:21:02 +0300

    - zipi

--- project/src/Map.js
+++ project/src/Map.js
@@ -1,7 +1,6 @@
 
 import MenuItem from '@material-ui/core/MenuItem';
 import React, { Component } from 'react';
-import Class from './Mobx/class'
 import {
   withGoogleMap,
   withScriptjs,
@@ -10,22 +9,30 @@ import {
   InfoWindow,
   Polyline
 } from 'react-google-maps';
-
+import Cables from './Mobx/cables'
 import PlacesAutocomplete, {
   geocodeByAddress,
   getLatLng
 } from 'react-places-autocomplete';
 import { Select } from '@material-ui/core';
+import {toJS} from 'mobx'
+import {observer} from 'mobx-react'
+import { red } from '@material-ui/core/colors';
+import {f} from './services/functions'
 
-class Map extends Component {
+export default observer( 
+   class Map extends Component {
 
   constructor(props) {
     super(props);
+    console.log("render")
     this.state = {
       isOpen: false,
-      coords: { lat: 18.55996, lng: -68.388832 },
+      coords: { lat: 20.55996, lng: -88.388832 },
       address: 'search',
+      addarker:Cables.addarker==null
     };
+    console.log(this.path,"path------------")
   }
 
   handleChange = address => {
@@ -70,14 +77,24 @@ class Map extends Component {
   style = {
     border: "#282c34 solid 1px"
   }
-  path = [
-    { lat: 32.0855141, lng: 34.8441714 },
-    { lat: 32.0859428, lng: 34.8442113 },
-  ];
+  // path = [
+  //   { lat: 18.55996, lng: -68.388832 },
+  //   { lat: 18.558028, lng: -68.388971 }
+    
+  // ];
+   path=toJS( Cables.cablesStringArr);
+   polylineClick(polyMouseEvent)
+   {
+     
+     console.log("polylineClick",polyMouseEvent.latLng.toJSON())
+     console.log(polyMouseEvent.latLng.toJSON())
+     console.log( "f",f(polyMouseEvent.latLng.toJSON()))
+   }
   render() {
-    const GoogleMapExample = withGoogleMap(props => (
+  const  GoogleMapExample = observer(   withGoogleMap(props => (
+    <>
       <GoogleMap defaultCenter={this.state.coords} defaultZoom={13}>
-        <Marker
+        {/* <Marker
           key={this.props.index}
           position={this.state.coords}
           onClick={() => this.handleToggleOpen()}
@@ -90,16 +107,37 @@ class Map extends Component {
               <span>This is InfoWindow message!</span>
             </InfoWindow>
           )}
-        </Marker>
-        <Polyline path={this.path} options={{ strokeColor: "#FF0000 " }} />
+        </Marker> */}
+        {Cables.markerAdd == null && <Marker
+          key={this.props.index}
+          position={{lat: 20.558377775557584, lng: -88.38892999428121}}
+          onClick={() => this.handleToggleOpen()}
+        >
+          {this.state.isOpen && (
+            <InfoWindow
+              onCloseClick={this.props.handleCloseCall}
+              options={{ maxWidth: 100 }}
+            >
+              <span>This is InfoWindow message!</span>
+            </InfoWindow>
+          )}
+        </Marker>}
+        {
+          this.path.map( pt =>
+             <Polyline path={pt} options={{ strokeColor: "#aqua " }} onClick={this.polylineClick}/>
+          )
+          }
+
       </GoogleMap>
-    ));
+      </>
+    )))
 
     const styleWidth = {
       width: "100%"
     }
     return (
-      <div>
+      <div style={{width:"50%"}}>
+
         <PlacesAutocomplete
           value={this.state.address}
           onChange={this.handleChange}
@@ -150,10 +188,10 @@ class Map extends Component {
           style={this.styleWidth}
           containerElement={<div style={{ height: `500px`, width: '100%' }} />}
           mapElement={<div style={{ height: `100%` }} />}
+          addMarker={Cables.markerAdd}
         />
       </div>
     );
   }
 }
-
-export default Map;
+)
